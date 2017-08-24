d2ee82eacd9dc6457ee6cf73ddf45bdd0a2d27e7
pdvega <pdvegaa@gmail.com>
2017-08-24 21:31:19 +0700

    update terakhir

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const FB = require('fb')
+const fb = new FB.Facebook({version: 'v2.8'})
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const setAccessToken = (req, res, next) => {
+  FB.setAccessToken(req.headers.fbaccesstoken);
+  next()
+}
+
+router.get('/facebook', setAccessToken, (req, res) => {
+  FB.api('/me', {field: ['id', 'name', 'email']}, function(response){
+    res.send(response)
+  })
+})
 
 module.exports = router;
